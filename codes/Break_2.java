cd45bbe485c9919b8cfb60558e9b8a2c854931a6
Sai Srihitha Koneti <99351904+Srihitha2@users.noreply.github.com>
2023-02-14 19:29:37 -0600

    Create Break_2.java

---
+++ codes/Break_2.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+class main {
+    public static void main (String[] args) {
+      int n = 1;
+      switch(n){
+        case 1:
+          System.out.println("First Case");
+          break;
+        case 2:
+          System.out.println("Second Case");
+          break;
+        default:
+          System.out.println("Default case");
+      }
+    }
+}
