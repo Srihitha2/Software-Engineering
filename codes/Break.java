6eca431d1c9b97a21384ab2d53ac98f4889d159a
Sai Srihitha Koneti <99351904+Srihitha2@users.noreply.github.com>
2023-02-15 14:41:47 -0600

    Update Break.java

--- codes/Break.java
+++ codes/Break.java
@@ -1,6 +1,10 @@
-for (int i = 0; i < 10; i++) {
-  if (i == 4) {
-    break;
-  }
-  System.out.println(i);
+public class Example {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
 }
