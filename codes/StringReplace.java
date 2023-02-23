a86872721465a9bc2b5b6ffd4ec1a65e0305b14f
Sai Srihitha Koneti <99351904+Srihitha2@users.noreply.github.com>
2023-02-23 11:27:41 -0600

    Create StringReplace.java

---
+++ codes/StringReplace.java
@@ -0,0 +1,8 @@
+public class StringReplace {
+    public static void main(String[] args) {
+        String str = "Hello, world!";
+        String replaced = str.replace('l', 'z');
+        System.out.println("Replaced string is: " + replaced);
+    }
+}
+//Replaced string is: Hezzo, worzd!
