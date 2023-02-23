d6cf4fc1e369012520310a6fc0c00ed06e86927f
Sai Srihitha Koneti <99351904+Srihitha2@users.noreply.github.com>
2023-02-23 11:24:18 -0600

    Create StringUpperCase.java

---
+++ codes/StringUpperCase.java
@@ -0,0 +1,8 @@
+public class StringToUpper {
+    public static void main(String[] args) {
+        String str = "Hello, world!";
+        String upper = str.toUpperCase();
+        System.out.println("Uppercase string is: " + upper);
+    }
+}
+//Uppercase string is: HELLO, WORLD!
