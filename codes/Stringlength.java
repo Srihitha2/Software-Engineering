c438293f84cbae5dba6ca484b4b301ad7c3e3d8c
Sai Srihitha Koneti <99351904+Srihitha2@users.noreply.github.com>
2023-02-23 11:19:52 -0600

    Create Stringlength.java

---
+++ codes/Stringlength.java
@@ -0,0 +1,10 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "Hello, world!";
+        int len = str.length();
+        System.out.println("Length of the string is: " + len);
+    }
+}
+
+
+//Length of the string is: 13
