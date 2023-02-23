5946231fda430fc8bd494901787ca4b74d8561f8
Sai Srihitha Koneti <99351904+Srihitha2@users.noreply.github.com>
2023-02-23 11:30:35 -0600

    Create Stringnew.java

---
+++ codes/Stringnew.java
@@ -0,0 +1,10 @@
+
+public class StringExample {
+    public static void main(String args[])
+    {
+        String s1 = new String("example");
+        // creating java string by new keyword
+        System.out.println(s1);
+    }
+}
+//example
