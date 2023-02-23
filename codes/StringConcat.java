4f018e9bcbdcd54eb80166ec40787bebf920a819
Sai Srihitha Koneti <99351904+Srihitha2@users.noreply.github.com>
2023-02-23 11:34:26 -0600

    Create StringConcat.java

---
+++ codes/StringConcat.java
@@ -0,0 +1,8 @@
+public class StringConcat {
+    public static void main(String[] args) {
+        String s = "Sachin";
+        s = s.concat(" Tendulkar");
+        System.out.println(s);
+    }
+}
+//Sachin Tendulkar
